02bbdc7b720dce605af69826b34e0577182bc888
telefon2232 <telefon2232@gmail.com>
2019-05-14 09:39:10 +0300

    update

--- create_xml.py
+++ create_xml.py
@@ -1,14 +1,14 @@
-import json
 from handler_data import handler_data
 from regular import handler_line
 from get_value import get_v
 
-def create_xml(text,text2):
+def create_xml(text2):
     z=handler_line(get_v())
   #  print(z)
     slov = {}
-    for i in range(len(z)):
+    len_z=len(z)
+    for i in range(len_z):
         slov[z[i]] = text2[i].split('\n')
     print(slov)
 
-create_xml(get_v(),handler_data())
+create_xml(handler_data())
